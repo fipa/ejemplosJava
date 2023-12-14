03f61d812849ee5ca94f0c3ea78367ef53497fd0
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-12-14 11:13:30 -0300

    Eliminando tests.

--- src/testDoubles/JugadorTest.java
+++ src/testDoubles/JugadorTest.java
@@ -16,41 +16,4 @@ class JugadorTest {
 		dadoStub = DadoStub.getInstance();
 	}
 	
-	@Test
-	public void testNuevoJugador() {
-		assertEquals("Homero", jugador.getNombre());
-		assertEquals(0, jugador.getPuntaje());
-		assertEquals(0,  jugador.getAccesorios().size());
-	}
-	
-	@Test
-	public void testJugadorAcumulaPuntajeAlTirarElDado() {
-		dadoStub.setValorEsperado(3);
-		jugador.lanzarDado(dadoStub);
-		assertEquals(3, jugador.getPuntaje());
-		dadoStub.setValorEsperado(6);
-		jugador.lanzarDado(dadoStub);
-		assertEquals(9, jugador.getPuntaje());
-	}
-	
-	@Test
-	public void testPuntajeDelJugadorSeDebeRestarDespueDeAdquirirUnAccesorio() {
-		dadoStub.setValorEsperado(3);
-		jugador.lanzarDado(dadoStub);
-		Accesorio a1 = new Accesorio("Lentes", 2);
-		assertTrue(jugador.adquirir(a1));
-		assertEquals(1,  jugador.getPuntaje());
-		assertSame(a1, jugador.getAccesorios().get(0));
-	}
-
-	@Test
-	public void testJugadorPuedeComprarAccesorioSoloCuandoLeAlcanzaElPuntaje() {
-		dadoStub.setValorEsperado(3);
-		jugador.lanzarDado(dadoStub);
-		Accesorio a1 = new Accesorio("Lentes", 10);
-		assertFalse(jugador.adquirir(a1));
-		assertEquals(3,  jugador.getPuntaje());
-		assertEquals(0,  jugador.getAccesorios().size());		
-	}
-
 }

9624815c91d263505b09c1454943e2d0b5344fdf
Fernando Poblete Arrau <fernandopobletearrau@gmail.com>
2023-12-14 09:22:43 -0300

    Subiendo unit test.

---
+++ src/unitTest/MathOperationsTest.java
@@ -0,0 +1,33 @@
+package unitTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MathOperationsTest {
+
+    @Test
+    public void itShouldSumAnArrayOfNumbers() {
+
+        // Arrange
+        Integer[] numbers = {1,2,3};
+
+        // Act
+        Integer result = MathOperations.sum(numbers);
+
+        // Assert
+        assertEquals(new Integer(6), result);
+
+    }
+
+    @Test
+    public void itShouldSumZeroForAnEmptyArray() {
+
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+}
